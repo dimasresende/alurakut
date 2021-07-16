4bdbda2ec6ebc31bd797d51feca68463a1cb88a6
Dimas Resende <dresende@uolinc.com>
2021-07-16 16:36:31 -0300

    Criando pagina de logout e removendo autenticacao

---
+++ pages/logout.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import nookies from 'nookies';
+
+export default function LogoutPage() {
+  const router = useRouter();
+
+  useEffect(async () => {
+    console.log('asdasd')
+
+    nookies.destroy(null, 'USER_TOKEN');
+    router.push('/login');
+
+  }, []);
+    
+  return (
+    <main style={{ display: 'flex', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <div className="loginScreen">
+        <section className="logoArea">
+          <img src="https://alurakut.vercel.app/logo.svg" />
+
+          <p><strong>Conecte-se</strong> aos seus amigos e familiares usando recados e mensagens instantâneas</p>
+          <p><strong>Conheça</strong> novas pessoas através de amigos de seus amigos e comunidades</p>
+          <p><strong>Compartilhe</strong> seus vídeos, fotos e paixões em um só lugar</p>
+        </section>
+
+        <section className="formArea">
+          <form className="box">
+            <p>
+              Estamos fazendo o logout do seu usuário do <strong>GitHub</strong>!
+          </p>
+          </form>
+
+        </section>
+
+        <footer className="footerArea">
+          <p>
+            © 2021 alura.com.br - <a href="/">Sobre o Orkut.br</a> - <a href="/">Centro de segurança</a> - <a href="/">Privacidade</a> - <a href="/">Termos</a> - <a href="/">Contato</a>
+          </p>
+        </footer>
+      </div>
+    </main>
+  )
+}
